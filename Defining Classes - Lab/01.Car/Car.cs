ace9f01f2a62470c226d949de15d8ad3faafbf3b
Yusein Bankeli <Bankeli995@gmail.com>
2025-09-26 20:09:45 +0300

    Defining Classes - Lab

---
+++ Defining Classes - Lab/01.Car/Car.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ Defining Classes - Lab/01.Car/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+           Car car = new Car
+           {
+               Make = "Audi",
+               Model = "A3",
+               Year = 2005
+           };
+
+            Console.WriteLine($"My car is : {car.Make} {car.Model} {car.Year} year.");
+        }
+    }
+}
